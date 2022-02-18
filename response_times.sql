84c7ce216320b68de82b3432ef92ae31d5175e53
laumc98 <lanaca98@gmail.com>
2022-02-18 18:37:19 -0500

    aplications-responsetimes

--- response_times.sql
+++ response_times.sql
@@ -3,12 +3,11 @@ select
    date(opportunities.reviewed) as approved_date,
    date(opportunity_candidates.interested) as interested_date,
    `Tracking Codes`.`utm_medium` as utm_medium,
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'srh_jobs') as 'Response Time Job Search',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'ja_mtc') as 'Response Time Job Alerts from Preferences Matches',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'rc_trrx_inv') as 'Response Time Manual invite by Torrex of registered candidates (internal sourcing)',
-    (select timestampdiff(day, approved_date,min(interested_date)) where interested_date is not null and utm_medium = 'rc_cb_rcdt') as 'Response Time Career Advisor'
+   min(IF (date(opportunities.reviewed) < date(opportunity_candidates.interested), timestampdiff(day, date(opportunities.reviewed),date(opportunity_candidates.interested)), (timestampdiff(day, date(opportunity_candidates.interested),date(opportunities.reviewed))*-1))) AS 'RT'
 FROM opportunity_candidates
 LEFT JOIN `tracking_code_candidates` `Tracking Code Candidates - Person` ON `opportunity_candidates`.`id` = `Tracking Code Candidates - Person`.`candidate_id`
 LEFT JOIN `tracking_codes` `Tracking Codes` ON `Tracking Code Candidates - Person`.`tracking_code_id` = `Tracking Codes`.`id`
 LEFT JOIN opportunities on opportunities.id = opportunity_candidates.opportunity_id
-group by  ID
+WHERE opportunity_candidates.interested is not null AND utm_medium IN ('srh_jobs','ja_mtc','rc_trrx_inv','rc_cb_rcdt')
+group by ID, utm_medium
+order by ID desc
