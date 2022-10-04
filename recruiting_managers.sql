79eedda1243eab9523ee648bd7f285c614d140e0
laumc98 <lanaca98@gmail.com>
2022-10-04 16:52:04 -0500

    Update recruiting_managers.sql

--- recruiting_managers.sql
+++ recruiting_managers.sql
@@ -1,7 +1,7 @@
 /* AA : Sonic : recruiting managers: prod */
 SELECT
     organization_service_providers.organization_id AS 'Company_id',
-    organization_service_providers.account_manager_person_gg_id AS 'RM'
+    organization_service_providers.account_manager_person_gg_id AS 'gg_id'
 FROM
     organization_service_providers
 GROUP BY 
