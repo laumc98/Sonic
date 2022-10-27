140bd6890737a3606dee700b036b928c715016f6
laumc98 <lanaca98@gmail.com>
2022-10-27 11:54:38 -0500

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -19,7 +19,7 @@ SELECT
    -- Created date 
     DATE(o.created) as 'Created date',
     -- Approved date
-    date(coalesce(null, o.first_reviewed, o.last_reviewed)) as 'Approved date',
+    coalesce(null, o.first_reviewed, o.last_reviewed) as 'Approved date',
     o.review as 'Approved',
     -- Applicant Acquisition Coordinator
     (select name FROM people p WHERE o.candidate_recruiter_person_id = p.id) as 'Candidate Recruiter',
