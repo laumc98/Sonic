c137801cc1cebef7ae38549cef1ee8001f677cb5
laumc98 <lanaca98@gmail.com>
2022-11-16 21:14:08 -0500

    Update notified_users.sql

--- notified_users.sql
+++ notified_users.sql
@@ -30,5 +30,6 @@ WHERE
             OR notifications.template = 'career-advisor-syndication-already-exist'
         )
         AND notifications.status = 'sent'
-        AND notifications.send_at > date(date_add(now(6), INTERVAL -7 month))
+        AND notifications.send_at > date(date_add(now(6), INTERVAL -5 month))
+        AND people.name NOT LIKE '**test*'
     )
