de69ff9359548a447b77a05f5d0f28a4f43a1938
laumc98 <lanaca98@gmail.com>
2022-11-23 10:42:18 -0500

    Create tasks.sql

---
+++ tasks.sql
@@ -0,0 +1,7 @@
+/* AA : Sonic : tasks by candidate recruiters: prod */ 
+SELECT
+    *
+FROM
+    tasks
+WHERE
+    tasks.assigned_to IN ('1227372','1070313','50406','1072288','163315','1148423','1062012','257012')
