f83cd1533a27638baee3e862a066d3dc80f0f0b6
laumc98 <lanaca98@gmail.com>
2022-11-30 18:11:37 -0500

    Update CR_mm.sql

--- CR_mm.sql
+++ CR_mm.sql
@@ -1,9 +1,9 @@
 /* AA : SONIC : mutual m by candidate recruiters : prod */ 
 SELECT
+    date(occh.created) AS 'mutualm_date',
+    p.name AS 'Candidate Recruiter',
     o.id AS 'ID',
-    occh.candidate_id AS 'candidate_id',
-    occh.created AS 'mutualm_date',
-    p.name AS 'Candidate Recruiter'
+    count(distinct occh.candidate_id) AS 'Mutual matches'
 FROM
     opportunity_candidate_column_history occh
     INNER JOIN opportunity_columns oc ON occh.to = oc.id
@@ -13,4 +13,7 @@ WHERE
     oc.name = 'mutual matches'
     AND occh.created >= '2022-10-23'
     AND o.candidate_recruiter_person_id IS NOT NULL
-ORDER BY occh.created ASC
+GROUP BY 
+    date(occh.created),
+    p.name,
+    o.id
