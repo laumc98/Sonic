80876c5c95070a969837b31e0c70713b4fed43d1
laumc98 <lanaca98@gmail.com>
2022-12-12 11:24:13 -0500

    Update tasks.sql

--- tasks.sql
+++ tasks.sql
@@ -1,7 +1,9 @@
 /* AA : Sonic : tasks by candidate recruiters: prod */ 
 SELECT
-    *
+    tasks.*,
+    opportunities.opportunity_id AS 'Alfa ID'
 FROM
     tasks
+    LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
 WHERE
     tasks.assigned_to IN ('1227372','1070313','50406','1072288','163315','1148423','1062012','257012')
