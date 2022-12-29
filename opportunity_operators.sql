f77637d713eae56e2055947501787d7cc3b871b5
laumc98 <lanaca98@gmail.com>
2022-12-29 10:37:11 -0500

    Create opportunity_operators.sql

---
+++ opportunity_operators.sql
@@ -0,0 +1,10 @@
+/* AA : Sonic : opportunity operators: prod */ 
+SELECT
+    opportunities.opportunity_id AS 'Alfa ID',
+    operators.operator_gg_id,
+    opportunity_operators.role,
+    operators.slack_id
+FROM 
+    opportunity_operators
+    LEFT JOIN opportunities ON opportunity_operators.opportunity_id = opportunities.id
+    LEFT JOIN operators ON opportunity_operators.operator_id = operators.id
