b9d55f67681a37e314a0f769c051a0ee1392f500
laumc98 <lanaca98@gmail.com>
2023-02-13 17:24:10 -0500

    Add daily app / channel

---
+++ daily_app_channel.sql
@@ -0,0 +1,19 @@
+/* AA : SONIC : daily app per channel : prod */ 
+SELECT
+    date(opportunity_candidates.interested) AS application_date,
+    opportunities.id AS ID,
+    tracking_codes.utm_medium AS utm_medium, 
+    count(*) AS applications
+FROM 
+    opportunity_candidates
+    INNER JOIN opportunities ON opportunity_candidates.opportunity_id = opportunities.id
+    LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
+    LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id
+WHERE 
+    opportunity_candidates.interested IS NOT NULL 
+    AND opportunities.review = 'approved'
+    AND DATE(opportunity_candidates.interested) > date(date_add(now(6), INTERVAL -1 month))
+GROUP BY 
+    date(opportunity_candidates.interested),
+    opportunities.id,
+    tracking_codes.utm_medium
--- recruiter_chatbot_ccg_notifications.sql
+++ recruiter_chatbot_ccg_notifications.sql
@@ -1,17 +1,21 @@
-/* AA : Sonic : ccg notifications: prod */ 
+/* AA : Sonic : ccg notifications: prod */
 SELECT
-    TRIM('"' FROM JSON_EXTRACT(`notifications`.`context`, '$.opportunityId')) as 'Alfa ID',
+    TRIM('"' FROM JSON_EXTRACT(notifications.context, '$.opportunityId')) as 'Alfa ID',
     count(*) as 'rc_ccg_notifications'
 FROM
-    `notifications`
-    LEFT JOIN `people` `People - To` ON `notifications`.`to` = `People - To`.`id`
+    notifications
+    LEFT JOIN people ON notifications.to = people.id
 WHERE
     (
-        (`notifications`.`template` = 'career-advisor-job-opportunity'
-            or `notifications`.`template` = 'career-advisor-invited-job-opportunity')
-        AND `notifications`.`status` = 'sent'
-        AND `People - To`.`subject_identifier` IS NULL
-        AND `People - To`.`name` not like '%test%'
+        (
+            notifications.template = 'career-advisor-job-opportunity'
+            or notifications.template = 'career-advisor-invited-job-opportunity'
+        )
+        AND notifications.status = 'sent'
+        AND (
+            people.subject_identifier IS NULL
+            OR people.subject_identifier != people.gg_id
+        )
+        AND date(notifications.sent_at) >= '2023-02-14'
+        AND people.name not like '%test%'
     )
-GROUP BY
-    1
