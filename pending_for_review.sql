aabd0d12091e7650a7dbb2f8804a2eeb8be1da9a
laumc98 <lanaca98@gmail.com>
2023-02-22 16:08:57 -0500

    Add # of pending for review/channel + deleted unused queries

---
+++ pending_for_review.sql
@@ -0,0 +1,22 @@
+/* AA : Sonic : # of pending for review users by utm: prod */ 
+SELECT 
+    opportunities.id,
+    tracking_codes.utm_medium,
+    count(distinct opportunity_candidates.id) AS pending_review
+FROM
+    opportunity_candidates
+    INNER JOIN opportunities ON opportunity_candidates.opportunity_id = opportunities.id
+    LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
+    LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id 
+    LEFT JOIN opportunity_candidate_column_history ON opportunity_candidates.id = opportunity_candidate_column_history.candidate_id
+    LEFT JOIN member_evaluations ON opportunity_candidates.id = member_evaluations.candidate_id
+    LEFT JOIN people ON opportunity_candidates.person_id = people.id
+WHERE
+    opportunity_candidates.interested IS NOT NULL
+    AND opportunity_candidate_column_history.created IS NULL
+    AND member_evaluations.not_interested IS NULL
+    AND opportunities.review = 'approved'
+    AND DATE(opportunities.last_reviewed) > date(date_add(now(6), INTERVAL -1 year))
+GROUP BY 
+    opportunities.id,
+    tracking_codes.utm_medium
