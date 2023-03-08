97d4628834a555a5281978de50c913f298e11d6b
laumc98 <lanaca98@gmail.com>
2023-03-08 11:16:28 -0500

    Query optimization

--- job_views.sql
+++ job_views.sql
@@ -5,16 +5,13 @@ select
 from
     views v
     inner join opportunities o on o.id = v.target_id
-    inner join opportunity_members omp on omp.opportunity_id = v.target_id
-    inner join person_flags pf on pf.person_id = omp.person_id
+    inner join opportunity_members omp on omp.opportunity_id = v.target_id and omp.poster = true
+    inner join person_flags pf on pf.person_id = omp.person_id and pf.opportunity_crawler = false
 where
-    o.objective not like '***%'
-    and omp.poster = true
-    and pf.opportunity_crawler = false
-    and v.target_type = 'opportunity'
-    and DATE(o.last_reviewed) > date(date_add(now(6), INTERVAL -1 year))
+    v.target_type = 'opportunity'
+    and DATE(v.created) > date(date_add(now(6), INTERVAL -1 year))
+    and o.objective not like '***%'
     and o.review = 'approved'
-    AND o.objective not like '**%'
     AND o.active = TRUE
 group by
     o.id
