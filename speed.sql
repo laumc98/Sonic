f2fcf0ba5669d69a8c3208cc02ac0ead77783546
laumc98 <lanaca98@gmail.com>
2023-03-08 17:03:40 -0500

    Queries refactoring

--- speed.sql
+++ speed.sql
@@ -1,62 +1,60 @@
 /* AA : Sonic : speed metrics: prod */ 
-SELECT
-    opportunities.id AS 'ID',
-    src_applications.first_app_date AS 'src_first_app_date',
-    syn_applications.first_app_date AS 'syn_first_app_date',
-    paid_syn_applications.first_app_date AS 'paid_syn_first_app_date'
-FROM 
-    opportunities
-    LEFT JOIN (
+SELECT 
+    semiraw.ID,
+    min(IF(
+        utm IN (
+            'syn_paid',
+            'rc_syn_paid',
+            'rc_syn_paid_trxx_inv'
+        ),
+        first_app_date,
+        NULL
+    )) AS paid_syn_first_app_date,
+    min(IF(
+        utm IN (
+            'syn',
+            'rc_syn',
+            'rc_syn_paid_trxx_inv'
+        ),
+        first_app_date,
+        NULL
+    )) AS syn_first_app_date,
+    min(IF(
+        utm IN (
+            'src',
+            'rc_src',
+            'rc_src_trxx_inv'
+        ),
+        first_app_date,
+        NULL
+    )) AS src_first_app_date
+FROM
+    (
         SELECT
             opportunity_candidates.opportunity_id AS 'ID',
+            tracking_codes.utm_medium AS utm,
             min(opportunity_candidates.interested) AS 'first_app_date'
-        FROM 
+        FROM
             opportunity_candidates
             LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
             LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id
         WHERE
-            opportunity_candidates.interested IS NOT NULL
-            AND (tracking_codes.utm_medium = 'rc_src'
-                    OR tracking_codes.utm_medium = 'rc_src_trxx_inv')
+            DATE(opportunity_candidates.created) >= date(date_add(now(6), INTERVAL -1 YEAR))
+            AND tracking_codes.utm_medium IN (
+                'syn_paid',
+                'rc_syn_paid',
+                'rc_syn_paid_trxx_inv',
+                'syn',
+                'rc_syn',
+                'rc_syn_paid_trxx_inv',
+                'src',
+                'rc_src',
+                'rc_src_trxx_inv'
+            )
         GROUP BY
-            opportunity_candidates.opportunity_id
-    ) AS src_applications ON opportunities.ID = src_applications.ID
-    LEFT JOIN (
-        SELECT
-            opportunity_candidates.opportunity_id AS 'ID',
-            min(opportunity_candidates.interested) AS 'first_app_date'
-        FROM 
-            opportunity_candidates
-            LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
-            LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id
-        WHERE
-            opportunity_candidates.interested IS NOT NULL
-            AND (tracking_codes.utm_medium = 'syn'
-                    OR tracking_codes.utm_medium = 'rc_syn'
-                    OR tracking_codes.utm_medium = 'rc_syn_trrx_inv')
-        GROUP BY
-            opportunity_candidates.opportunity_id
-    ) AS syn_applications ON opportunities.ID = syn_applications.ID
-    LEFT JOIN (
-        SELECT
-            opportunity_candidates.opportunity_id AS 'ID',
-            min(opportunity_candidates.interested) AS 'first_app_date'
-        FROM 
-            opportunity_candidates
-            LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
-            LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id
-        WHERE
-            opportunity_candidates.interested IS NOT NULL
-            AND (tracking_codes.utm_medium = 'syn_paid'
-                    OR tracking_codes.utm_medium = 'rc_syn_paid'
-                    OR tracking_codes.utm_medium = 'rc_syn_paid_trxx_inv')
-        GROUP BY
-            opportunity_candidates.opportunity_id
-    ) AS paid_syn_applications ON opportunities.ID = paid_syn_applications.ID
-WHERE
-    opportunities.review = 'approved'
-    AND opportunities.active = TRUE
-    AND opportunities.candidate_recruiter_person_id IS NOT NULL
-    AND DATE(opportunities.last_reviewed) > date(date_add(now(6), INTERVAL -1 year))
+            opportunity_candidates.opportunity_id,
+            utm
+    ) semiraw
+    INNER JOIN opportunities ON semiraw.ID = opportunities.id AND opportunities.review = 'approved'
 GROUP BY 
-    opportunities.id
+    semiraw.ID
