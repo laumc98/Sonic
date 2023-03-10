82c6c1a3b6b93178dad47c90635db615c38ddfb3
laumc98 <lanaca98@gmail.com>
2023-03-10 15:30:43 -0500

    Revert "Update tasks.sql"
    This reverts commit 6476b58bb0ee68d7e5622eb2b6d211afb105bfe0.

--- tasks.sql
+++ tasks.sql
@@ -5,3 +5,5 @@ SELECT
 FROM
     tasks
     LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
+WHERE
+    tasks.assigned_to IN ('1227372','1070313','50406','1072288','163315','1148423','1062012','257012')
