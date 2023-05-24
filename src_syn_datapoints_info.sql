f867a8c1953504df20996b5e3e0985ab2a38a191
laumc98 <lanaca98@gmail.com>
2023-05-24 15:43:58 -0500

    Create src_syn_datapoints_info.sql

---
+++ src_syn_datapoints_info.sql
@@ -0,0 +1,46 @@
+/* AA : Sonic : sourcing & syndication info from data points: prod */
+SELECT
+    o.opportunity_id,
+    REGEXP_SUBSTR(src_datapoint.comments, '(?<=Number of connections found: \[)\d+(?=\])') AS src_connections_found,
+    REGEXP_SUBSTR(src_datapoint.comments, '(?<=Number of connections needed: \[)\d+(?=\])') AS src_connections_needed,
+    REGEXP_SUBSTR(syn_datapoint.comments, '(?<=Applications needed: \[)\d+(?=\])') AS syn_applications_needed
+FROM 
+    opportunities o
+    LEFT JOIN (
+        SELECT
+            d.opportunity_id,
+            d.comments
+        FROM 
+            data_points d
+        WHERE 
+            d.comments IS NOT NULL
+            AND d.id IN (
+                SELECT 
+                    MAX(id)
+                FROM 
+                    data_points
+                WHERE
+                    subject = 'sourcing-feedback'
+                GROUP BY 
+                    opportunity_id
+            )
+    ) src_datapoint ON o.id = src_datapoint.opportunity_id
+    LEFT JOIN (
+        SELECT
+            d.opportunity_id,
+            d.comments
+        FROM 
+            data_points d
+        WHERE 
+            d.comments IS NOT NULL
+            AND d.id IN (
+                SELECT 
+                    MAX(id)
+                FROM 
+                    data_points
+                WHERE
+                    subject = 'syndication-log'
+                GROUP BY 
+                    opportunity_id
+            )
+    ) syn_datapoint ON o.id = syn_datapoint.opportunity_id
