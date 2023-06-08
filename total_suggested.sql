7c0d6778a1968506874d77d2b8880b3a97c323b0
laumc98 <lanaca98@gmail.com>
2023-06-08 14:13:45 -0500

    Update total_suggested.sql

--- total_suggested.sql
+++ total_suggested.sql
@@ -1,12 +1,14 @@
 /* AA : Sonic : total suggested: prod */
 select
-   o.id as 'ID',
-   count(distinct p.username) as 'Suggested'
+   op.opportunity_id as 'ID',
+   count(distinct op.person_id) as 'Suggested'
 from
    opportunity_suggestions op
    inner join opportunities o on op.opportunity_id = o.id
-   inner join people p on op.person_id = p.id
 where
    DATE(o.last_reviewed) > date(date_add(now(6), INTERVAL -1 year))
+   AND o.objective not like '**%'
+   AND o.active = TRUE
+   AND o.review = 'approved'
 group by
-   opportunity_id
+   op.opportunity_id
