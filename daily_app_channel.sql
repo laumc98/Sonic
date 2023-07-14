e4d92f3d8a0fb7103dbe4ca5c95b184b9eda2b9f
laumc98 <lanaca98@gmail.com>
2023-07-14 10:48:04 -0500

    Update daily_app_channel.sql

--- daily_app_channel.sql
+++ daily_app_channel.sql
@@ -1,22 +1,26 @@
 /* AA : SONIC : daily app per channel : prod */ 
 SELECT
+    date(opportunity_candidates.created) AS started_date, 
     date(opportunity_candidates.interested) AS application_date,
     opportunities.id AS ID,
-    tracking_codes.utm_medium AS utm_medium, 
+    IF(ISNULL(opportunity_candidates.interested), 'started', 'finished') AS status,
+    tracking_codes.utm_medium AS utm_medium,
     tracking_codes.utm_source AS utm_source,
     count(distinct opportunity_candidates.id) AS applications
-FROM 
+FROM
     opportunity_candidates
     INNER JOIN opportunities ON opportunity_candidates.opportunity_id = opportunities.id
     LEFT JOIN tracking_code_candidates ON opportunity_candidates.id = tracking_code_candidates.candidate_id
     LEFT JOIN tracking_codes ON tracking_code_candidates.tracking_code_id = tracking_codes.id
-WHERE 
-    opportunity_candidates.interested IS NOT NULL
-    AND opportunity_candidates.application_step IS NOT NULL
+WHERE
+    opportunity_candidates.application_step IS NOT NULL
     AND opportunities.review = 'approved'
-    AND DATE(opportunity_candidates.interested) > date(date_add(now(6), INTERVAL -3 month))
+    AND DATE(opportunity_candidates.created) >= date(date_add(now(6), INTERVAL -3 month))
+    AND opportunities.id = 1884566
 GROUP BY 
+    date(opportunity_candidates.created),
     date(opportunity_candidates.interested),
     opportunities.id,
+    IF(ISNULL(opportunity_candidates.interested), 'started', 'finished'),
     tracking_codes.utm_medium,
     tracking_codes.utm_source
