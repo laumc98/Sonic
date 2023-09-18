e6b0847d2cb7873c9d70d8c5863d921f15a314a6
laumc98 <lanaca98@gmail.com>
2023-09-18 14:50:50 -0500

    Update tasks.sql

--- tasks.sql
+++ tasks.sql
@@ -6,7 +6,14 @@ FROM
     tasks
     LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
 WHERE
-    (tasks.completed_by IN ('1164202','1227372','50406','1465392','1072288','163315','1148423','257012','1171237','1135927','1328036','1201446','1072337','1205587','218202','879186','1217202','1558464','46223')
-        OR tasks.completed_by IS NULL)
+    tasks.completed_by IN ('1164202','1227372','50406','1465392','1072288','163315','1148423','257012','1171237','1135927','1328036','1201446','1072337','1205587','218202','879186','1217202','1558464','46223')
     AND tasks.trigger IN ('paid-ext-networks','ext-networks','ext-sourcing','manual-task-creation','recruiter-bot-conversation-email','recruiter-bot-conversation-messenger','recruiter-bot-conversation-whatsapp','recruiter-bot-manual-reassign','otto-recruiter-bot-email','otto-recruiter-bot-messenger','otto-recruiter-bot-whatsapp')
     AND tasks.created >= '2023-01-01'
+    AND tasks.id NOT IN (
+        SELECT 
+            tasks.parent_task_id
+        FROM 
+            tasks  
+        WHERE 
+            tasks.trigger = 'inspection-task-completed-without-execution'
+    )
