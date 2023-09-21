659016399b8b4379e9efd8920fbab636f3928386
laumc98 <lanaca98@gmail.com>
2023-09-21 09:31:38 -0500

    Add new triggers

--- tasks.sql
+++ tasks.sql
@@ -7,7 +7,7 @@ FROM
     LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
 WHERE
     tasks.completed_by IN ('1164202','1227372','50406','1465392','1072288','163315','1148423','257012','1171237','1135927','1328036','1201446','1072337','1205587','218202','879186','1217202','1558464','46223')
-    AND tasks.trigger IN ('paid-ext-networks','ext-networks','ext-sourcing','manual-task-creation','recruiter-bot-conversation-email','recruiter-bot-conversation-messenger','recruiter-bot-conversation-whatsapp','recruiter-bot-manual-reassign','otto-recruiter-bot-email','otto-recruiter-bot-messenger','otto-recruiter-bot-whatsapp')
+    AND tasks.trigger IN ('paid-ext-networks','paid-ext-networks-pro','paid-ext-networks-ss','ext-networks','ext-networks-pro','ext-networks-ss','ext-sourcing','ext-sourcing-pro','ext-sourcing-ss','manual-task-creation','recruiter-bot-conversation-email','recruiter-bot-conversation-messenger','recruiter-bot-conversation-whatsapp','recruiter-bot-manual-reassign','otto-recruiter-bot-email','otto-recruiter-bot-messenger','otto-recruiter-bot-whatsapp')
     AND tasks.created >= '2023-01-01'
     AND tasks.id NOT IN (
         SELECT 
