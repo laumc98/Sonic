f8563641d98239d5a8e03d8ddbfdc68d811aa9e2
laumc98 <lanaca98@gmail.com>
2023-11-02 14:50:43 -0500

    Update CR_mm.sql

--- CR_mm.sql
+++ CR_mm.sql
@@ -1,74 +1,63 @@
 /* AA : SONIC : mutual m by candidate recruiters : prod */ 
 SELECT
-    date(mutual_matches.created) AS 'mutualm_date',
-    mutual_matches.utm_campaign AS 'cr_campaign',
-    mutual_matches.utm_medium,
-    mutual_matches.id AS 'ID',
-    count(*) AS 'Mutual matches'
+    date(applications.timestamp) AS 'mutualm_date',
+    applications.opportunity_reference_id AS 'Alfa ID',
+    applications.utm_medium AS 'utm_medium',
+    applications.utm_campaign AS 'cr_campaign',
+    count(*) AS 'unique_hqa_mm'
 FROM
-(
-    SELECT
-        occh.candidate_id,
-        min(occh.created) AS 'created',
-        tc.utm_campaign,
-        tc.utm_medium,
-        o.id
-    FROM
-        opportunity_candidate_column_history occh
-        INNER JOIN opportunity_columns oc ON occh.to = oc.id
-        INNER JOIN opportunities o ON oc.opportunity_id = o.id
-        LEFT JOIN opportunity_candidates oca ON occh.candidate_id = oca.id
-        LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
-        LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
-        LEFT JOIN people p ON o.candidate_recruiter_person_id = p.id
-    WHERE
-        oc.name = 'mutual matches'
-        AND occh.created >= '2022-10-25'
-        AND (tc.utm_campaign = 'amdm'
-            OR tc.utm_campaign = 'mcog'
-            OR tc.utm_campaign = 'dffa'
-            OR tc.utm_campaign = 'czp'
-            OR tc.utm_campaign = 'jdpb'
-            OR tc.utm_campaign = 'dmc'
-            OR tc.utm_campaign = 'nsr'
-            OR tc.utm_campaign = 'mmor'
-            OR tc.utm_campaign = 'JAMC'
-            OR tc.utm_campaign = 'mgdd'
-            OR tc.utm_campaign = 'mrh'
-            OR tc.utm_campaign = 'srl'
-            OR tc.utm_campaign = 'avs'
-            OR tc.utm_campaign = 'sbr'
-            OR tc.utm_campaign = 'tavp'
-            OR tc.utm_campaign = 'rmr' 
-            OR tc.utm_campaign = 'dgv'
-            OR tc.utm_campaign = 'MER' 
-            OR tc.utm_campaign = 'ACMP'
-            OR tc.utm_campaign = 'dgc'
-            OR tc.utm_campaign = 'fcr'
-            OR tc.utm_campaign = 'mes'
-            OR tc.utm_campaign = 'mcmn'
-            OR tc.utm_campaign = 'mfo'
-            OR tc.utm_campaign = 'smfp'
-            OR tc.utm_campaign = 'gebj'
-            OR tc.utm_campaign = 'aamf'
-            OR tc.utm_campaign = 'eb'
-            OR tc.utm_campaign = 'kglm'
-            OR tc.utm_campaign = 'sm'
-            OR tc.utm_campaign = 'brc'
-            OR tc.utm_campaign = 'vaio'
-            OR tc.utm_campaign = 'exrm'
-            OR tc.utm_campaign = 'jsmn'
-            OR tc.utm_campaign = 'lfas'
-            OR tc.utm_campaign = 'malm'
-            OR tc.utm_campaign = 'lbmp'
-            OR tc.utm_campaign = 'capi'
-        )
-        AND tc.utm_medium IN ('src','rc_src','rc_src_trxx_inv','syn','rc_syn','rc_syn_trrx_inv','syn_paid','rc_syn_paid','rc_syn_paid_trrx_inv','syn_rqt','rc_syn_rqt')
-    GROUP BY 
-        occh.candidate_id
-) AS mutual_matches
-GROUP BY 
-    date(mutual_matches.created),
-    mutual_matches.utm_campaign,
-    mutual_matches.utm_medium,
-    mutual_matches.id
+    applications
+    LEFT JOIN opportunity ON applications.opportunity_reference_id = opportunity.ref_id
+    LEFT JOIN mutual_matches ON (mutual_matches.gg_id = applications.gg_id AND mutual_matches.opportunity_reference_id = applications.opportunity_reference_id)
+WHERE
+    (applications.filters_passed = true
+    OR mutual_matches.timestamp IS NOT NULL)
+    AND (applications.utm_campaign = 'amdm'
+        OR applications.utm_campaign = 'mcog'
+        OR applications.utm_campaign = 'dffa'
+        OR applications.utm_campaign = 'czp'
+        OR applications.utm_campaign = 'jdpb'
+        OR applications.utm_campaign = 'dmc'
+        OR applications.utm_campaign = 'nsr'
+        OR applications.utm_campaign = 'mmor'
+        OR applications.utm_campaign = 'JAMC'
+        OR applications.utm_campaign = 'mgdd'
+        OR applications.utm_campaign = 'mrh'
+        OR applications.utm_campaign = 'srl'
+        OR applications.utm_campaign = 'avs'
+        OR applications.utm_campaign = 'sbr'
+        OR applications.utm_campaign = 'tavp'
+        OR applications.utm_campaign = 'rmr' 
+        OR applications.utm_campaign = 'dgv'
+        OR applications.utm_campaign = 'MER' 
+        OR applications.utm_campaign = 'ACMP'
+        OR applications.utm_campaign = 'dgc'
+        OR applications.utm_campaign = 'fcr'
+        OR applications.utm_campaign = 'mes'
+        OR applications.utm_campaign = 'mcmn'
+        OR applications.utm_campaign = 'mfo'
+        OR applications.utm_campaign = 'smfp'
+        OR applications.utm_campaign = 'gebj'
+        OR applications.utm_campaign = 'aamf'
+        OR applications.utm_campaign = 'eb'
+        OR applications.utm_campaign = 'kglm'
+        OR applications.utm_campaign = 'sm'
+        OR applications.utm_campaign = 'brc'
+        OR applications.utm_campaign = 'vaio'
+        OR applications.utm_campaign = 'exrm'
+        OR applications.utm_campaign = 'jsmn'
+        OR applications.utm_campaign = 'lfas'
+        OR applications.utm_campaign = 'malm'
+        OR applications.utm_campaign = 'lbmp'
+        OR applications.utm_campaign = 'capi'
+        OR applications.utm_campaign = 'cals'
+        OR applications.utm_campaign = 'bb'
+        OR applications.utm_campaign = 'jcmv'
+        OR applications.utm_campaign = 'egc'
+    )
+    AND applications.utm_medium IN ('src','rc_src','rc_src_trrx_inv','syn','rc_syn','rc_syn_trrx_inv','syn_paid','rc_syn_paid','rc_syn_paid_trrx_inv','syn_rqt','rc_syn_rqt')
+GROUP BY
+    date(applications.timestamp),
+    applications.opportunity_reference_id,
+    applications.utm_medium,
+    applications.utm_campaign
