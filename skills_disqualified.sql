b30ec8ddfa7446bb31127c493f055ecfcb4371b1
laumc98 <lanaca98@gmail.com>
2023-11-07 20:32:42 -0500

    Create skills_disqualified.sql

---
+++ skills_disqualified.sql
@@ -0,0 +1,13 @@
+/* AA : Sonic : skills disqualified: prod */ 
+SELECT 
+    member_evaluations.candidate_id, 
+    member_evaluations_reason.reason,
+    member_evaluations_reason.code AS 'Skill ID'
+FROM 
+    member_evaluations_reason
+    LEFT JOIN member_evaluations ON member_evaluations_reason.member_evaluation_id = member_evaluations.id
+WHERE 
+    member_evaluations_reason.reason = 'skills-required'
+    AND (member_evaluations_reason.created >= date(date_add(now(6), INTERVAL -60 day))
+            AND member_evaluations_reason.created < date(date_add(now(6), INTERVAL 1 day)))
+    AND member_evaluations_reason.active = true
