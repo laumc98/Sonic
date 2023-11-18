f82fdfd5b4d90628c278648574bd38d04f8f3773
laumc98 <lanaca98@gmail.com>
2023-11-18 09:19:27 -0500

    Create src_connections.sql

---
+++ src_connections.sql
@@ -0,0 +1,21 @@
+/* AA : Sonic : src connections: prod */
+SELECT 
+    opportunities.opportunity_id AS 'Alfa ID',
+    IF(
+        ISNULL(tasks.completed), 
+        'Requests_Not_Completed', 
+        'Requests_Completed'
+    ) AS task_status,
+    count(tasks.id) AS number_tasks
+FROM 
+    tasks
+    LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
+WHERE 
+    (tasks.trigger = 'sourcing-connection-request'
+        OR tasks.trigger = 'sourcing-connection-request-agile'
+        OR tasks.trigger = 'sourcing-connection-request-boost'
+        OR tasks.trigger = 'sourcing-connection-request-pro'
+        OR tasks.trigger = 'sourcing-connection-request-ss')
+GROUP BY 
+   opportunities.opportunity_id,
+   task_status
