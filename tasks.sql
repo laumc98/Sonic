58a57a596a4c7b0818220d07883e30250616bee4
laumc98 <lanaca98@gmail.com>
2023-12-26 10:48:19 -0500

    Update tasks.sql

--- tasks.sql
+++ tasks.sql
@@ -6,7 +6,7 @@ FROM
     tasks
     LEFT JOIN opportunities ON tasks.opportunity_id = opportunities.id
 WHERE
-    tasks.completed_by IN ('1164202','50406','1072288','163315','1135927','1072337','1205587','218202','879186','1217202','1558464','46223','1485590','1446790','1566080','1558709','1573613','1576877','1514679','1578431','709519','275690','1461284','1494405','2b7d8bed692749fd8e698aad0bd424b6','1109045','1373785','1392028','1575208','31195','1558904','1185377','1558542','1334308','1568148','1183460','1632179','1233729','1199351','1558306','1603354','36942','e7c625164d7e49738f8fc15f5ce85796')       
+    tasks.completed_by IN ('1164202','50406','1072288','163315','1135927','1072337','1205587','218202','879186','1217202','1558464','46223','1485590','1446790','1566080','1558709','1573613','1576877','1514679','1578431','709519','275690','1461284','1494405','2b7d8bed692749fd8e698aad0bd424b6','1109045','1373785','1392028','1575208','31195','1558904','1185377','1558542','1334308','1568148','1183460','1233729','1199351','1558306','1603354','36942','e7c625164d7e49738f8fc15f5ce85796','1426784')       
     AND tasks.trigger IN ('paid-ext-networks','paid-ext-networks-pro','paid-ext-networks-ss','ext-networks','ext-networks-pro','ext-networks-ss','ext-sourcing','ext-sourcing-pro','ext-sourcing-ss','manual-task-creation','recruiter-bot-conversation-email','recruiter-bot-conversation-messenger','recruiter-bot-conversation-whatsapp','recruiter-bot-manual-reassign','otto-recruiter-bot-email','otto-recruiter-bot-messenger','otto-recruiter-bot-whatsapp')
     AND tasks.created >= '2023-01-01'
     AND tasks.id NOT IN (
