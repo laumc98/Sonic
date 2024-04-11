e05a278ded945136412ed237029d7c64a6b7c83f
laumc98 <lanaca98@gmail.com>
2024-04-10 19:30:01 -0500

    Update CR_mm.sql

--- CR_mm.sql
+++ CR_mm.sql
@@ -55,6 +55,7 @@ WHERE
         OR applications.utm_campaign = 'jcmv'
         OR applications.utm_campaign = 'egc'
         OR applications.utm_campaign = 'mdr'
+        OR applications.utm_campaign = 'grtt'
     )
     AND applications.utm_medium IN ('src','rc_src','rc_src_trrx_inv','syn','rc_syn','rc_syn_trrx_inv','syn_paid','rc_syn_paid','rc_syn_paid_trrx_inv','syn_rqt','rc_syn_rqt')
 GROUP BY
