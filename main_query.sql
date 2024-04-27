60e3b5611f3cd9eff08cc88cb399c5704cc61d1e
laumc98 <lanaca98@gmail.com>
2024-04-27 13:27:25 -0500

    Update services definition

--- main_query.sql
+++ main_query.sql
@@ -17,38 +17,43 @@ opps_services AS (
         CASE
             WHEN crawled THEN 'crawled'
             WHEN (
-                 FIND_IN_SET('1', gs.services)>0 /* agile */
-                 OR FIND_IN_SET('7', gs.services)>0 /* staff augmentation */
-                 OR FIND_IN_SET('20', gs.services)>0 /* hunt */
+                (
+                    FIND_IN_SET('1', gs.services)>0 /* agile */
+                    OR FIND_IN_SET('7', gs.services)>0 /* staff augmentation */
+                    OR FIND_IN_SET('20', gs.services)>0 /* hunt */
+                )
             ) THEN 'rpo'
             WHEN (
-                FIND_IN_SET('8', gs.services)>0 /* ats */
-                OR FIND_IN_SET('9', gs.services)>0 /* pro */
-                OR FIND_IN_SET('16', gs.services)>0 /* torre_os */
+                (
+                    FIND_IN_SET('8', gs.services)>0 /* ats */
+                    OR FIND_IN_SET('9', gs.services)>0 /* pro */
+                    OR FIND_IN_SET('16', gs.services)>0 /* torre_os */
+                ) AND o.created>='2023-12-01'
             ) THEN 'torre_os'
             WHEN (
                 (
-                 FIND_IN_SET('2', gs.services)>0 /* essentials */
-                 OR FIND_IN_SET('6', gs.services)>0 /* self service */
-                 OR services = ''
-                 OR services IS NULL
-                 )
+                    FIND_IN_SET('2', gs.services)>0 /* essentials */
+                    OR FIND_IN_SET('6', gs.services)>0 /* self service */
+                    OR services = ''
+                    OR services IS NULL
+                )  AND o.created>='2023-12-01'
             ) THEN 'torre_free'
-            WHEN (
-                FIND_IN_SET('11', gs.services)>0 /* boost */
-                OR FIND_IN_SET('12', gs.services)>0 /* boost hqa */
-            ) THEN 'boost'
             ELSE 'others'
         END AS business_line,
         (
-            FIND_IN_SET('17', gs.services)>0 /* torre_reach_essential */
-            OR FIND_IN_SET('18', gs.services)>0 /* torre_reach_syndication */
-            OR FIND_IN_SET('19', gs.services)>0 /* torre_reach_sourcing */
+            (
+                FIND_IN_SET('11', gs.services)>0 /* boost */
+                OR FIND_IN_SET('12', gs.services)>0 /* boost hqa */
+                OR FIND_IN_SET('17', gs.services)>0 /* torre_reach_essential */
+                OR FIND_IN_SET('18', gs.services)>0 /* torre_reach_syndication */
+                OR FIND_IN_SET('19', gs.services)>0 /* torre_reach_sourcing */
+            ) AND o.created>='2023-12-01'
         ) AS reach,
         services
     FROM
-        groupped_services gs
-        INNER JOIN opportunities o ON gs.opportunity_id=o.id
+        opportunities o
+    LEFT JOIN groupped_services gs ON
+        gs.opportunity_id = o.id
 ), main_opps AS (
 SELECT
     -- ID
