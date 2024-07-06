feee7716b50981d53e516414d8c75ac37b8602e5
laumc98 <lanaca98@gmail.com>
2024-07-05 20:59:24 -0500

    Update niagara.sql

--- niagara.sql
+++ niagara.sql
@@ -1,5 +1,5 @@
 SELECT
-    o.ref_id AS 'Alfa ID',
+    o.ref_id AS "Alfa ID",
     o.updated_on AS opportunity_updated_on,
     o.compensation_status,
     o.target_hires,
@@ -19,14 +19,14 @@ SELECT
     s.reason_changed,
     s.last_state_transition
 FROM
-    opportunity AS o
+    puzzles.opportunity AS o
     LEFT JOIN (
         SELECT
             opportunity_reference_id,
-            GROUP_CONCAT(channel SEPARATOR ', ') as channels,
+            LISTAGG(channel,', ') WITHIN GROUP (ORDER BY opportunity_reference_id) as channels,
             MIN(created) as channels_open_date
         FROM
-            opportunity_channels
+            puzzles.opportunity_channels
         WHERE
             active = 1
         GROUP BY
@@ -35,14 +35,14 @@ FROM
     LEFT JOIN (
         SELECT
             opportunity_reference_id,
-            MIN(CASE WHEN channel = 'EXTERNAL_NETWORKS' THEN created END) AS 'syn_activation_date',
-            MAX(CASE WHEN channel = 'EXTERNAL_NETWORKS' THEN created END) AS 'max_syn_activation_date',
-            MIN(CASE WHEN channel = 'EXTERNAL_SOURCING' THEN created END) AS 'src_activation_date',
-            MAX(CASE WHEN channel = 'EXTERNAL_SOURCING' THEN created END) AS 'max_src_activation_date',
-            MIN(CASE WHEN channel = 'PAID_EXTERNAL_NETWORK' THEN created END) AS 'paid_syn_activation_date',
-            MAX(CASE WHEN channel = 'PAID_EXTERNAL_NETWORK' THEN created END) AS 'max_paid_syn_activation_date'
+            MIN(CASE WHEN channel = 'EXTERNAL_NETWORKS' THEN created END) AS "syn_activation_date",
+            MAX(CASE WHEN channel = 'EXTERNAL_NETWORKS' THEN created END) AS "max_syn_activation_date",
+            MIN(CASE WHEN channel = 'EXTERNAL_SOURCING' THEN created END) AS "src_activation_date",
+            MAX(CASE WHEN channel = 'EXTERNAL_SOURCING' THEN created END) AS "max_src_activation_date",
+            MIN(CASE WHEN channel = 'PAID_EXTERNAL_NETWORK' THEN created END) AS "paid_syn_activation_date",
+            MAX(CASE WHEN channel = 'PAID_EXTERNAL_NETWORK' THEN created END) AS "max_paid_syn_activation_date"
         FROM
-            opportunity_channels
+            puzzles.opportunity_channels
         GROUP BY
             opportunity_reference_id
     ) activation ON o.ref_id = activation.opportunity_reference_id
@@ -54,40 +54,39 @@ FROM
             st.reason_changed AS reason_changed,
             ps.last_previous_state
         FROM
-            state_transition AS st
+            puzzles.state_transition AS st
             LEFT JOIN (
                 SELECT
                     state_transition.opportunity_reference_id,
                     state_transition.current_state AS last_previous_state
                 FROM
-                    state_transition
+                    puzzles.state_transition 
                 WHERE
                     state_transition.active = FALSE
                     AND state_transition.id IN (
                         SELECT 
                             MAX(state_transition.id)
                         FROM
-                            state_transition
+                            puzzles.state_transition
                         WHERE   
                             state_transition.active = FALSE
                         GROUP BY 
                             state_transition.opportunity_reference_id
                     )
                 GROUP BY
-                    state_transition.opportunity_reference_id
+                    state_transition.opportunity_reference_id,
+                    state_transition.current_state
             ) ps ON st.opportunity_reference_id = ps.opportunity_reference_id
         WHERE
             st.active = TRUE
-        GROUP BY 
-            st.opportunity_reference_id
     ) s ON o.ref_id = s.opportunity_reference_id
     LEFT JOIN (
         SELECT
             v.opportunity_reference_id AS opportunity_reference_id,
             COUNT(a.id) / COUNT(v.opportunity_reference_id) AS torre_alerts_views_to_application_ratio
         FROM
-            views AS v
-            LEFT JOIN applications AS a ON v.gg_id = a.gg_id
+            puzzles.views AS v
+            LEFT JOIN puzzles.applications AS a ON v.gg_id = a.gg_id
             AND v.opportunity_reference_id = a.opportunity_reference_id
         WHERE
             v.utm_medium = 'ja_mtc'
@@ -100,9 +99,9 @@ FROM
             COUNT(a.id) / COUNT(v.opportunity_reference_id) AS torre_handled_views_to_application_ratio_since_update,
             COUNT(v.opportunity_reference_id) AS views_since_update
         FROM
-            views AS v
-            LEFT JOIN opportunity AS opp ON v.opportunity_reference_id = opp.ref_id
-            LEFT JOIN applications AS a ON v.gg_id = a.gg_id
+            puzzles.views AS v
+            LEFT JOIN puzzles.opportunity AS opp ON v.opportunity_reference_id = opp.ref_id
+            LEFT JOIN puzzles.applications AS a ON v.gg_id = a.gg_id
             AND v.opportunity_reference_id = a.opportunity_reference_id
             AND a.timestamp >= opp.updated_on
         WHERE
@@ -116,7 +115,5 @@ FROM
             v.opportunity_reference_id
     ) thvtat ON o.ref_id = thvtat.opportunity_reference_id
 WHERE
-    JSON_EXTRACT(o.opportunity_snapshot, '$."crawled"') = FALSE
-    AND (o.created >= STR_TO_DATE(CONCAT(DATE_FORMAT(DATE_ADD(NOW(6), INTERVAL -3 month), '%Y-%m'), '-01'), '%Y-%m-%d'))
-GROUP BY
-    o.ref_id
+    o.created >= current_date - interval '365 days'
+    AND lower(o.opportunity_snapshot) LIKE '%"crawled": false%'
